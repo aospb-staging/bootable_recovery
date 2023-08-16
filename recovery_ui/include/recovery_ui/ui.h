553aaa64e31ecb664ace8dfffe4122424a2d0618
Joseph Annareddy <javelinanddart@gmail.com>
2023-08-16 12:04:56 -0400

    recovery: Bypass reboot prompt for sideload-auto-reboot
    When we want to automatically reboot after a sideload, it doesn't make
    much sense to try and prompt the user for a reboot into recovery.
    
    Change-Id: Id0195965362b62bf940caba1f83ffe12191a73c6

--- recovery_ui/include/recovery_ui/ui.h
+++ recovery_ui/include/recovery_ui/ui.h
@@ -208,6 +208,14 @@ class RecoveryUI {
 
   virtual bool IsUsbConnected();
 
+  bool IsSideloadAutoReboot() const {
+    return sideload_auto_reboot_;
+  }
+
+  void SetSideloadAutoReboot(bool sar) {
+    sideload_auto_reboot_ = sar;
+  }
+
  protected:
   void EnqueueKey(int key_code);
 
@@ -223,6 +231,8 @@ class RecoveryUI {
   bool touch_screen_allowed_;
 
   bool fastbootd_logo_enabled_;
+
+  bool sideload_auto_reboot_;
 
  private:
   enum class ScreensaverState {
--- recovery_ui/ui.cpp
+++ recovery_ui/ui.cpp
@@ -69,6 +69,7 @@ RecoveryUI::RecoveryUI()
                                                       DEFAULT_MAX_BRIGHTNESS_FILE)),
       touch_screen_allowed_(false),
       fastbootd_logo_enabled_(false),
+      sideload_auto_reboot_(false),
       touch_low_threshold_(android::base::GetIntProperty("ro.recovery.ui.touch_low_threshold",
                                                          kDefaultTouchLowThreshold)),
       touch_high_threshold_(android::base::GetIntProperty("ro.recovery.ui.touch_high_threshold",
